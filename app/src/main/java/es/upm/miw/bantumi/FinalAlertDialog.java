b2769fc1c926510f19ee4af402d2fe5bca9cbd4e
Javier Gil <franciscojavier.gil@upm.es>
2023-10-14 16:59:32 +0200

    Inicio proyecto

---
+++ app/src/main/java/es/upm/miw/bantumi/FinalAlertDialog.java
@@ -0,0 +1,44 @@
+package es.upm.miw.bantumi;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class FinalAlertDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+	public AppCompatDialog onCreateDialog(Bundle savedInstanceState) {
+		final MainActivity main = (MainActivity) requireActivity();
+
+        assert main != null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(main);
+        builder
+                .setTitle(R.string.txtDialogoFinalTitulo)
+                .setMessage(R.string.txtDialogoFinalPregunta)
+                .setPositiveButton(
+                        getString(android.R.string.ok),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                main.juegoBantumi.inicializar(JuegoBantumi.Turno.turnoJ1);
+                            }
+                        }
+                )
+                .setNegativeButton(
+                        getString(android.R.string.cancel),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                main.finish();
+                            }
+                        }
+                );
+
+		return builder.create();
+	}
+}
